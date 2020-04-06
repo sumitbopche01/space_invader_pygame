466627dadbaa6f6137bf26516f00f90604afb9e8
Sumit Bopche <sumit@freightbro.com>
2020-04-07 01:42:26 +0530

    added keyboard controls

--- main.py
+++ main.py
@@ -15,19 +15,28 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('airplane.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
-
+def player(x,y):
+    screen.blit(playerImg, (x, y))
 
 running = True
 while running:
-
     screen.fill((0, 0, 0))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        #if kestroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
-    player()
+    playerX += playerX_change
+    
+    player(playerX, playerY)
     pygame.display.update()
